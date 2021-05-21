0a2afb436f4ccbe706de8a5b8f6ee993b8a8013c
Lincoln <lincolninsper@gmail.com>
2021-05-21 20:20:28 -0300

    definição de estado e tela de ataque selecionado

--- Jogo/Codigo.py
+++ Jogo/Codigo.py
@@ -12,6 +12,9 @@ defesa = pygame.image.load("imagens/defesa.jpeg")
 defesa = pygame.transform.scale(defesa, (ESPESSURA,170))
 fugir = pygame.image.load("imagens/fugir.jpeg")
 fugir = pygame.transform.scale(fugir, (ESPESSURA,170))
+atacar1 = pygame.image.load("imagens/Atacar.png")
+atacar1 = pygame.transform.scale(atacar1, (ESPESSURA,170))
+
 game = True # condicao para o jogo continuar rodando
 
 clock = pygame.time.Clock()
@@ -19,6 +22,8 @@ FPS = 30 #define velocidade dos quadros do jogo
 ATAQUE = 0
 DEFESA = 1
 FUGIR = 2
+ATACAR = 3
+ESTADO = ATAQUE
 while game:
     clock.tick(FPS)
     # ---- verifica os eventos que acontecem no jogo
@@ -31,7 +36,9 @@ while game:
             if event.key == pygame.K_UP:
                 ESTADO = FUGIR
             if event.key == pygame.K_DOWN:
-                ESTADO = DEFESA 
+                ESTADO = DEFESA
+            if event.key == pygame.K_RIGHT:
+                ESTADO = ATACAR
         elif event.type == pygame.KEYUP and ESTADO == DEFESA:
             if event.key == pygame.K_UP:
                 ESTADO = ATAQUE
@@ -41,6 +48,9 @@ while game:
             if event.key == pygame.K_UP:
                 ESTADO = DEFESA
             if event.key == pygame.K_DOWN:
+                ESTADO = ATAQUE
+        elif event.type == pygame.KEYUP and ESTADO == ATACAR:
+            if event.key == pygame.K_LEFT:
                 ESTADO = ATAQUE
 
     # atualiza a caixa de diálogo
@@ -57,6 +67,10 @@ while game:
             window.fill((0, 0, 0))  
             window.blit(background, (0, 0))
             window.blit(fugir,(0,430))
+    elif ESTADO == ATACAR:
+            window.fill((0, 0, 0))  
+            window.blit(background, (0, 0))
+            window.blit(atacar1,(0,430))
 
 
     pygame.display.update()
